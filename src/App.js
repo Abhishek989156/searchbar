38b7320a4fa5299eb26dca48b9c63bbbec09b083
Abhishek Sharma <monuuu0@gmail.com>
2024-01-27 14:26:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  
+  const[input,setInput]=useState([])
+  const[va,setVa]=useState("")
+ 
+ 
+async function check(e){
+    setVa(e)
+    const result= await fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then((data)=>{
+      const suggestions=data.products.filter((user)=>{
+        return (
+          e && user && user.title&& user.title.toLowerCase().includes(e)
+        );
+
+      });
+     
+      setInput(suggestions)
+     
+    })
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='box'>
+
+    
+      <div className='inpt'>
+        <input value={va} placeholder='Search' onChange={(e)=>check(e.target.value)}></input>
+        <button>Search</button>
+       </div>
+      <ul >
+        {
+        input.map((val,x)=>{
+         return <li onClick={(e)=>{
+         
+          setVa(val.title)
+
+        }} key={x}>{val.title}</li>
+
+        })
+      }
+      </ul> 
+      
+      
+  </div>
+
+      
     </div>
   );
 }
